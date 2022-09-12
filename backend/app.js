8a932f9971cd8416911edd156b45cc1bcd6bb8cc
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-09-12 20:14:36 +0000

    post event functionality added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+var cors = require("cors");
+const helmet = require("helmet");
+const nodemailer = require("nodemailer");
 const { Schema, model } = mongoose;
+
+require("dotenv").config();
 
 mongoose.connect("mongodb://localhost:27017/umat_venue_booking_db", {
   useNewUrlParser: true,
@@ -10,13 +15,14 @@ mongoose.connect("mongodb://localhost:27017/umat_venue_booking_db", {
 const db = mongoose.connection;
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
 
 const eventSchema = new Schema({
   title: String,
   date: Date,
   location: String,
-  time: Date,
   duration: Number,
   contact_person: String,
   phone_no: String,
@@ -26,34 +32,101 @@ const eventSchema = new Schema({
 
 const Event = model("Event", eventSchema);
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "mquarcoo@kehillahglobal.com",
+    pass: process.env.PASSWORD,
+  },
+});
+
 app.get("/events", (req, res) => {
-  Event.find({}, (err, events) => {
-    res.send({
-      success: true,
-      events,
+  try {
+    Event.find({}, (err, events) => {
+      res.send({
+        success: true,
+        events,
+      });
     });
-  });
+  } catch (error) {
+    res.send({
+      success: false,
+      error,
+    });
+  }
 });
 
 app.post("/events", (req, res) => {
-  // TODO: Change event items to req.body.<param_name>
-  const event = new Event({
-    ...req.body,
-  });
+  try {
+    const event = new Event({
+      title: req.body.title,
+      date: req.body.date,
+      location: req.body.location,
+      duration: req.body.duration,
+      contact_person: req.body.contact_person,
+      phone_no: req.body.phone_no,
+      email: req.body.email,
+      additional_info: req.body.additional_info,
+    });
 
-  event.save((err) => {
-    if (err) {
-      res.send({
-        success: false,
-        event,
-      });
-    } else {
-      res.send({
-        success: true,
-        event,
-      });
-    }
-  });
+    // Send email
+    const mailOptions = {
+      from: "mquarcoo@kehillahglobal.com",
+      to: "michaelquarcoo04@gmail.com, " + event.email,
+      subject: `Venue Booking request for ${location}`,
+      html: `<div>
+              <h1>Venue Booking Request</h1> 
+              <br /> 
+              <h3>Event Details</h3>
+              <p>Event Name: ${event.title}</p>
+              <p>Date: ${new Date(event.date).toDateString()}</p>
+              <p>Time: ${new Date(event.date).toTimeString()}</p>
+              <p>Duration: ${event.duration} hours</p>
+              <p>
+                Additional Information: $
+                {event.additional_info ? event.additional_info : "None"}
+              </p>
+              <br />
+              <br />
+              <h3>Contact Details</h3>
+              <p>Name: ${event.contact_person}</p>
+              <p>Phone Number: ${event.phone_no}</p>
+              <p>Email Address: ${event.email}</p>
+            </div>`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        res.send({
+          success: false,
+          error,
+        });
+        return;
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+
+    event.save((err) => {
+      if (err) {
+        res.send({
+          success: false,
+          event,
+          error: err,
+        });
+      } else {
+        res.send({
+          success: true,
+          event,
+        });
+      }
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      error,
+    });
+  }
 });
 
 app.listen(3000, () => {
--- frontend/js/book_venue.js
+++ frontend/js/book_venue.js
@@ -12,21 +12,31 @@ if (venue === "main_auditorium" || venue === "mini_auditorium") {
   locationTxtField.defaultValue =
     venue === "main_auditorium" ? "Main Auditorium" : "Mini Auditorium";
   const form = document.getElementById("form");
-  form.addEventListener("submit", (event) => {
+  form.addEventListener("submit", async (event) => {
     event.preventDefault();
-    const data = {
+    const payload = {
       title: form.elements["event_name"].value,
       date: form.elements["event_date"].value,
       location:
         venue === "main_auditorium" ? "Main Auditorium" : "Mini Auditorium",
-      time: form.elements["event_time"].value,
       duration: form.elements["event_duration"].value,
       contact_person: form.elements["name"].value,
       phone_no: form.elements["phone_no"].value,
       email: form.elements["email"].value,
       additional_info: form.elements["additional_info"].value,
     };
-    console.log(data);
+
+    const result = await fetch("http://localhost:3000/events", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
+
+    const data = await result.json();
+    if (data.success) {
+      alert("Request for venue placed successfully!");
+      window.location.href = "./";
+    }
   });
 } else {
   alert("Venue does not exist or is not available!");
